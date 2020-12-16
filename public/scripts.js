2a4a85f1d02086d896737aaf21fbe18dd896699b
Matheus-souza-araujo <matheus1mrs@gmail.com>
2020-12-16 00:02:56 -0300

    Realizando algoritmo de paginação no front, ainda não finalizado

--- public/scripts.js
+++ public/scripts.js
@@ -6,3 +6,25 @@ for (item of menuItens){
         item.classList.add("active")
     }
 }
+
+//lógica de como apresentar os núemeros de qual página está
+let totalPages = 20,
+    selectedPage = 15,
+    pages = [],
+    oldpage
+
+for (let currentPage = 1; currentPage <= totalPages; currentPage++){
+
+    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+    const pagesBeforeSelectedPag = currentPage>= selectedPage - 2
+
+    if(firstAndLastPage || pagesBeforeSelectedPag && pagesAfterSelectedPage){
+        pages.push(currentPage)
+
+
+        oldpage = currentPage
+    }
+}
+
+console.log(pages)
