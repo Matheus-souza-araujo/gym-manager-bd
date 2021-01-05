b648162518f77a5973051707fd08a2ea2e0913d7
Matheus-souza-araujo <matheus1mrs@gmail.com>
2021-01-05 00:49:59 -0300

    Finalizado paginação no front, agora falta somente estilizar

--- public/scripts.js
+++ public/scripts.js
@@ -1,37 +1,55 @@
-const currentPage= location.pathname
+const currentPage = location.pathname
 const menuItens = document.querySelectorAll("header .links a")
 
-for (item of menuItens){
-    if(currentPage.includes(item.getAttribute("href"))){
+for (item of menuItens) {
+    if (currentPage.includes(item.getAttribute("href"))) {
         item.classList.add("active")
     }
 }
 
-//lógica de como apresentar os núemeros de qual página está
-function pagination(selectedPage, totalPages){
+//lógica de como apresentar os números de qual página está
+function paginate(selectedPage, totalPages) {
     let pages = [],
-    oldPage
+        oldPage
 
-for (let currentPage = 1; currentPage <= totalPages; currentPage++){
+    for (let currentPage = 1; currentPage <= totalPages; currentPage++) {
 
-    const firstAndLastPage = currentPage == 1 || currentPage == totalPages
-    const pagesAfterSelectedPage = currentPage <= selectedPage + 2
-    const pagesBeforeSelectedPage = currentPage>= selectedPage - 2
+        const firstAndLastPage = currentPage == 1 || currentPage == totalPages
+        const pagesAfterSelectedPage = currentPage <= selectedPage + 2
+        const pagesBeforeSelectedPage = currentPage >= selectedPage - 2
 
-    if(firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage){
-        if (oldPage && currentPage - oldPage > 2) {
-            pages.push("...")
+        if (firstAndLastPage || pagesBeforeSelectedPage && pagesAfterSelectedPage) {
+            if (oldPage && currentPage - oldPage > 2) {
+                pages.push("...")//O método push() adiciona um ou mais elementos ao final de um array e retorna o novo
+            }
+
+            if (oldPage && currentPage - oldPage == 2) {
+                pages.push(oldPage + 1)
+            }
+
+            pages.push(currentPage)
+
+            oldPage = currentPage
         }
-
-        if (oldPage && currentPage - oldPage == 2) {
-            pages.push(oldPage + 1)
-        }
-
-        pages.push(currentPage)
-
-        oldPage = currentPage
     }
+
+    return pages;
 }
 
-return pages;
+const pagination = document.querySelector(".pagination")
+const page = +pagination.dataset.page
+const total = +pagination.dataset.total
+const pages = paginate(page, total)
+
+let elements = ""
+
+for (let page of pages) {
+    if(String(page).includes("...")){
+        elements += `<span>${page}</span>`
+    }else{
+        elements += `<a href="?page=${page}">${page}</a>`
+    }
+   
 }
+
+pagination.innerHTML = elements
