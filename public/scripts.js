9efe3f85a584468232e5fc4295c44a55c1941cce
Matheus-souza-araujo <matheus1mrs@gmail.com>
2021-01-18 20:06:42 -0300

    Finalizado criação do gerenciador de alunos, acadêmia. Porém ainda há um erro na busca

--- public/scripts.js
+++ public/scripts.js
@@ -36,20 +36,33 @@ function paginate(selectedPage, totalPages) {
     return pages;
 }
 
-const pagination = document.querySelector(".pagination")
-const page = +pagination.dataset.page
-const total = +pagination.dataset.total
-const pages = paginate(page, total)
 
-let elements = ""
+function createPagination(pagination) {
 
-for (let page of pages) {
-    if(String(page).includes("...")){
-        elements += `<span>${page}</span>`
-    }else{
-        elements += `<a href="?page=${page}">${page}</a>`
+    const filter = pagination.dataset.filter
+    const page = +pagination.dataset.page
+    const total = +pagination.dataset.total
+    const pages = paginate(page, total)
+
+    let elements = ""
+
+    for (let page of pages) {
+        if (String(page).includes("...")) {
+            elements += `<span>${page}</span>`
+        } else {
+            if (filter) {
+                elements += `<a href="?page=${page}&filter=${filter}">${page}</a>`
+            }
+            elements += `<a href="?page=${page}">${page}</a>`
+        }
+
     }
-   
+
+    pagination.innerHTML = elements
 }
 
-pagination.innerHTML = elements
+const pagination = document.querySelector(".pagination")
+
+if (pagination) {
+    createPagination(pagination)
+}
